a3375b3e939843f71fca86ffe7dc573471be9a71
miltonayala <miltonayala1704@gmail.com>
2025-10-11 21:47:17 -0600

    missing file

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-function main() {
-    console.log("Funcion principal para lo que sea XD");
+function getDate() { //Para obtener el mes para el titulo y tambien hacer la primera letra en mayuscula
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString('es-ES', {year: 'numeric', month: 'long'});
+    const capitalizedDate = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    console.log(capitalizedDate);
+    return capitalizedDate;
 }
+
+getDate();
